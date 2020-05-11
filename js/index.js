d5d7166440077b83126250d913fd6028a75b6875
dinhcaradoc <kwambugualex@gmail.com>
2020-05-11 10:17:50 +0300

    make changes to the functions and add final html

--- js/index.js
+++ js/index.js
@@ -1,39 +1,43 @@
 var maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
-var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
-var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']
+      var femaleNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+      var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday']
 
-function getDayofWeek(){
-  var dob = document.getElementById('dateOfBirth');
-  return dob;
-
-  var day = dob.getDate();
-  var month = dob.getMonth();
-  var year = dob.getYear();
-  var cent = year.substr(0,2);
-  var ury = year.substr(-3, 2);
-
-
-  d = (((cent/4)-2)*(cent-1) + (5*ury/4) + (26*(month+1)/10) + day)%7;
-  return d;
-};
-
-function findAkanName(){
-  var male = document.getElementById('maleRadio');
-  var female = document.getElementById('femaleRadio');
-  var dayOfWeek = days[d];
-  var akanName = document.getElementById('akanName');
-
-  if (male){
-    name = maleNames[d];
-  }
-  else {
-    name = femaleNames[d];
-  }
-  return name;
-  
-  akanName.innerhtml = "You were born on " + dayOfWeek + " and your Akan Name is" + name;
-}
-
- 
+      function getDayofWeek(){
+        var dob = parseInt(document.getElementById('dateOfBirth').value);
+        
+        var day = parseInt(dob.getDate());
+        var month = parseInt(dob.getMonth());
+        var year = parseInt(dob.getYear());
+        var cent = parseInt(year.substr(0,2));
+        var ury = parseInt(year.substr(-3, 2));
 
 
+        d = (((cent/4)-2)*(cent-1) + (5*ury/4) + (26*(month+1)/10) + day)%7;
+        return d;
+      };
+
+      function findAkanName(){
+        var dayOfWeek = days[d];
+        var gender = findGender();
+        var name = "";
+
+        if (gender ==="male"){
+            name = maleNames[d];
+          } 
+          else {
+            name = femaleNames[d];
+          }
+          //concat the day born and the Akan name
+          document.getElementById('akanName').innerhtml = "You were born on " + dayOfWeek + " and your Akan Name is" + name;
+      }
+
+      function findGender(){
+        var genderR = document.getElementsByName('gender');
+
+        for (var i = 0, length = genderR.length; i < length; i++) {
+          if (genderR[i].checked) {
+            gender = genderR[i].value;
+            return gender;
+          };
+          } 
+      }
